d441035807fb732a41103db72621ae4920d0aa51
Sophia Abdullaeva <sophiaabd97@gmail.com>
2024-10-19 15:26:57 +0300

    Add files via upload

---
+++ 1-2 ОПД/lab1/task.sh
@@ -0,0 +1,96 @@
+mkdir ~/lab0
+cd ~/lab0
+touch dugtrio4
+echo "Способности Growl Astonish Mud-Slap Magnitude Bulldoze
+Sucker Punch Sand Tomb Mud Bomb Earth Power Dig Slash Earthquake
+Fissure" > dugtrio4
+mkdir ~/lab0/hitmontop6
+cd ~/lab0/hitmontop6
+mkdir ~/lab0/hitmontop6/wigglytuff
+touch zigzagoon
+echo "Тип покемона NORMAL NONE" > zigzagoon
+touch carvanha
+echo "Тип диеты
+Carnivore" > carvanha
+mkdir ~/lab0/hitmontop6/tyrogue
+cd ..
+mkdir ~/lab0/nuzleaf4
+cd ~/lab0/nuzleaf4
+touch klink
+echo "Способности Vicegrip Charge Thundershock Gear Grind
+Bind Charge Beam Autotomize Mirror Shot Screech Discharge Metal Sound
+Shift Gear Lock-On Zap Cannon Hyper Beam" > klink
+touch cherrim
+echo "Ходы Bullet Seed
+Giga Drain Helping Hand Natural Gift Rollout Seed Bomb Sleep Talk
+Snore Synthesis Worry Seed" > cherrim
+touch skuntank
+echo "weight=83.8 height=39.0 atk=9
+def=7" > skuntank
+mkdir ~/lab0/nuzleaf4/octillery
+touch nidorino
+echo "Ходы Body Slam Counter Defense Curl Double-Edge Drill
+Run Helping Hand Iron Tail Mud-Slap Shock Wave Sleep Talk Snore Sucker
+Punch Super Fang Water Pulse" > nidorino
+touch wormadam
+echo "Развитые способности
+Overcoat" > wormadam
+cd ..
+touch seadra9
+echo "Ходы Bounce Dive Double-Edge Dragon Pulse Icy Wind
+Outrage Signal Beam Sleep Talk Snore Swift Twister Water
+Pulse" > seadra9
+cd ..
+mkdir ~/lab0/sunflora0
+cd ~/lab0/sunflora0
+mkdir ~/lab0/sunflora0/marill
+mkdir ~/lab0/sunflora0/ferrothorn
+touch excadrill
+echo "Возможности Overland=6 Surface=4 Burrow=10 Jump=3
+Power=3 Intelligence=4 Groundshaper=0" > excadrill
+cd ..
+touch tyrogue0
+echo "weight=46.3
+height=28.0 atk=4 def=4" > tyrogue0
+chmod 644 ~/lab0/dugtrio4
+chmod 573 ~/lab0/hitmontop6
+chmod 736 ~/lab0/hitmontop6/wigglytuff
+chmod 622 ~/lab0/hitmontop6/zigzagoon
+chmod 664 ~/lab0/hitmontop6/carvanha
+chmod 512 ~/lab0/hitmontop6/tyrogue
+chmod 736 ~/lab0/nuzleaf4
+chmod 404 ~/lab0/nuzleaf4/klink
+chmod 440 ~/lab0/nuzleaf4/cherrim
+chmod 624 ~/lab0/nuzleaf4/skuntank
+chmod 513 ~/lab0/nuzleaf4/octillery
+chmod 620 ~/lab0/nuzleaf4/nidorino
+chmod 622 ~/lab0/nuzleaf4/wormadam
+chmod 444 ~/lab0/seadra9
+chmod 733 ~/lab0/sunflora0
+chmod 375 ~/lab0/sunflora0/marill
+chmod 752 ~/lab0/sunflora0/ferrothorn
+chmod 046 ~/lab0/sunflora0/excadrill
+chmod 600 ~/lab0/tyrogue0
+ln seadra9 ~/lab0/nuzleaf4/cherrimseadra
+chmod u+r ~/lab0/sunflora0/excadrill
+cat ~/lab0/sunflora0/excadrill ~/lab0/nuzleaf4/wormadam >> ~/lab0/seadra9_55
+chmod u-r ~/lab0/sunflora0/excadrill
+chmod u+w ~/lab0/nuzleaf4/cherrimseadra
+cat seadra9 > ~/lab0/nuzleaf4/cherrimseadra
+chmod u-w ~/lab0/nuzleaf4/cherrimseadra
+chmod u+r ~/lab0/sunflora0/excadrill
+chmod u+r ~/lab0/sunflora0/marill
+cp -r ~/lab0/sunflora0 ~/lab0/hitmontop6/wigglytuff
+chmod u-r ~/lab0/sunflora0/excadrill
+chmod u-r ~/lab0/sunflora0/marill
+wc -l seadra9 > /tmp/total 2>&1
+ls -R -ltr | grep "n$" 2>/tmp/errors
+cat n* */n* */*/n* | sort -r 2>/dev/null
+ls -l | tail -n2 | grep "^n" | sort -k2 2>&1
+ls -R | grep "tyro" | sort -k5
+ls -1 sunflora0 | sort -r
+rm ~/lab0/dugtrio4
+rm ~/lab0/nuzleaf4/cherrim
+rm ~/lab0/nuzleaf4/klinksead*
+rm ~/lab0/nuzleaf4/cherrimsead*
+rm -rf ~/lab0/sunflora0
